ed5f7973a39bba450e3f9d18f5b891b3e674da88
firlysetya93 <113082391+firlysetya93@users.noreply.github.com>
2025-07-08 14:32:55 +0700

    1 streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import RobustScaler
+from sklearn.cluster import MeanShift
+from sklearn.metrics import davies_bouldin_score, silhouette_score
+from mpl_toolkits.mplot3d import Axes3D
 
+st.set_page_config(layout="wide")
+st.title("Aplikasi Pengelompokan Wilayah Berdasarkan Capaian Pengelolaan Sampah")
