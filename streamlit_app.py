b7193192e3bac433f61f5280c41a8b7c7a4a1ce9
firlysetya93 <113082391+firlysetya93@users.noreply.github.com>
2025-07-13 21:23:15 +0700

    101streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import io
-import tensorflow as tf
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.tsa.stattools import adfuller
